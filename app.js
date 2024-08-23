2923aefe4d542c62b734b61bea4429b1ef588e38
kiran-jawale <kiran464jawale@gmail.com>
2024-08-23 17:43:02 +0530

    aug 23 pm 5:40

--- app.js
+++ app.js
@@ -1,35 +1,72 @@
  
  
  //navbar//
-/*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
-
-/* Menu show */
-if(navToggle){
-   navToggle.addEventListener('click', () =>{
-      navMenu.classList.add('show-menu')
-   })
-}
-
-/* Menu hidden */
-if(navClose){
-   navClose.addEventListener('click', () =>{
-      navMenu.classList.remove('show-menu')
-   })
-}
+ var menu = document.querySelector("#nav i")
+ var cross = document.querySelector("#full i")
 
 
- //navbar End//
- //image slider//
+ var tl =gsap.timeline()
+  
+ tl.to("#full",{
+right:0,
+duration:0.5
+
+ })
+
+ tl.from("#full h4",{
+
+   x:150,
+   duration:0.5,
+   stagger:0.3,
+   opacity:0
+ })
+
+ tl.from("#full i",{
+opacity:0
+ })
+
+tl.pause()
+
  
+ menu.addEventListener("click",function(){
+tl.play()
 
- //image slider end// 
- // 3d image slider left to right // 
+ })
+ cross.addEventListener("click",function(){
+  tl.reverse()
 
-
-
+ })
+ //navbar End//
+ //image slider// 
+ var swiper = new Swiper(".mySwiper", {
+   effect: "coverflow",
+   grabCursor: true,
+   centeredSlides: true,
+   slidesPerView: "auto",
+   loop: true,
+   autoplay: {
+     delay: 3000,
+     disableOnInteraction: false,
+     pauseOnMouseEnter: true
+   },
+   speed: 600,
+   coverflowEffect: {
+     rotate: 50,
+     stretch: -20,
+     depth: 100,
+     modifier: 1,
+     slideShadows: true,
+   },
+   pagination: {
+     el: ".swiper-pagination",
+     clickable: true, 
+     dynamicBullets: true
+   },
+   navigation: {
+     nextEl: ".swiper-button-next",
+     prevEl: ".swiper-button-prev",
+   }
+ });
  // 3d image slider left to right End // 
  // 3d image slider up to down // 
 
