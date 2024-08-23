4ffd5c768b65d87936685b0fa7a1628ec9122dca
notyetsaurabh <notyetsaurabh@gmail.com>
2024-08-23 19:16:08 +0530

    23 aug  7.15pm

--- app.js
+++ app.js
@@ -78,6 +78,29 @@ tl.play()
 
  // Company numbers End // 
  // testmonial// 
+ 
+$('.testimonials-container').owlCarousel({
+  loop:true,
+  autoplay:true,
+  autoplayTimeout:6000,
+  margin:10,
+  nav:true,
+  navText:["<i class='fa-solid fa-arrow-left'></i>",
+           "<i class='fa-solid fa-arrow-right'></i>"],
+  responsive:{
+      0:{
+          items:1,
+          nav:false
+      },
+      600:{
+          items:1,
+          nav:true
+      },
+      768:{
+          items:2
+      },
+  }
+})
 
 
  // testmonial end// 
