4f81d3bd3dabdc4a2412e151202122d9e1621598
kiran-jawale <kiran464jawale@gmail.com>
2024-08-23 20:03:50 +0530

    aug 23 pm 8

--- app.js
+++ app.js
@@ -4,38 +4,33 @@
  var menu = document.querySelector("#nav i")
  var cross = document.querySelector("#full i")
 
+ var tl = gsap.timeline();
 
- var tl =gsap.timeline()
-  
- tl.to("#full",{
-right:0,
-duration:0.5
-
- })
-
- tl.from("#full h4",{
-
-   x:150,
-   duration:0.5,
-   stagger:0.3,
-   opacity:0
- })
-
- tl.from("#full i",{
-opacity:0
- })
-
-tl.pause()
-
+ tl.to("#full", {
+   right: 0,
+   duration: 0.2,
+ });
  
- menu.addEventListener("click",function(){
-tl.play()
-
- })
- cross.addEventListener("click",function(){
-  tl.reverse()
-
- })
+ tl.from("#full h4", {
+   x: 150,
+   duration: 0.3,
+   stagger: 0.2,
+   opacity: 0,
+ });
+ 
+ tl.from("#full i", {
+   opacity: 0,
+   duration: 0.1,
+ });
+ 
+ tl.pause();
+ 
+ menu.addEventListener("click", function () {
+   tl.play();
+ });
+ cross.addEventListener("click", function () {
+   tl.reverse();
+ });
  //navbar End//
  //image slider// 
  var swiper = new Swiper(".mySwiper", {
