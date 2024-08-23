288ae91cbfd9e676c79af166231a5f6b6dc4a273
kiran-jawale <kiran464jawale@gmail.com>
2024-08-23 13:39:02 +0530

    23 aug pm 1.38

---
+++ app.js
@@ -0,0 +1,50 @@
+ 
+ 
+ //navbar//
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Menu show */
+if(navToggle){
+   navToggle.addEventListener('click', () =>{
+      navMenu.classList.add('show-menu')
+   })
+}
+
+/* Menu hidden */
+if(navClose){
+   navClose.addEventListener('click', () =>{
+      navMenu.classList.remove('show-menu')
+   })
+}
+
+
+ //navbar End//
+ //image slider//
+ 
+
+ //image slider end// 
+ // 3d image slider left to right // 
+
+
+
+ // 3d image slider left to right End // 
+ // 3d image slider up to down // 
+
+
+
+ // 3d image slider up to down end // 
+ // Company numbers  // 
+
+
+ // Company numbers End // 
+ // testmonial// 
+
+
+ // testmonial end// 
+ //footer//
+
+
+ //footer End//
