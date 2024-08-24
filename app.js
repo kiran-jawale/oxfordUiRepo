75949d62493b981b4ace67ab772ff63e9cdd5e7f
kiran-jawale <kiran464jawale@gmail.com>
2024-08-24 19:04:20 +0530

    aug 23 pm 7:00

--- app.js
+++ app.js
@@ -74,7 +74,45 @@
 
  // 3d image slider up to down // 
 
+   /** image slider up to down  */
 
+   var tl = gsap.timeline({scrollTrigger:{
+    trigger:"#updown",
+     markers:true,
+    start:"38% 50%",
+    end:"100% 50%",
+    scrub:2,
+    pin:true
+}});
+
+
+tl
+.to(".text",{
+    top: "-7%",
+},'a')
+.to("#card-one",{
+    top: "35%",
+},'a')
+.to("#card-two",{
+    top: "130%"
+},'a')
+.to("#card-two",{
+    top: "42%"
+},'b')
+.to("#card-one",{
+    width: "65%",
+    height: "65vh"
+},'b')
+.to("#card-three",{
+    top: "130%"
+}, 'b')
+.to("#card-three",{
+    top: "50%"
+}, 'c')
+.to("#card-two",{
+    width: "70%",
+    height: "70vh"
+},'c')
 
  // 3d image slider up to down end // 
  // Company numbers  // 
